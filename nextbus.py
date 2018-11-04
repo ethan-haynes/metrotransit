81d12c21f94ce80da230d10e86a17a141a056cbb
Ethan Haynes <eghaynes17@gmail.com>
2018-11-04 11:26:39 -0600

    wrapped script in class

--- nextbus.py
+++ nextbus.py
@@ -3,45 +3,69 @@ from difflib import SequenceMatcher
 
 diff = lambda a, b: SequenceMatcher(None, a, b).ratio()
 
-try:
-    if len(sys.argv) != 4: raise ValueError
-    
-    _, route, stop, direction = sys.argv
-
-    conn = http.client.HTTPConnection('svc.metrotransit.org')
-    conn.request("GET", "/NexTrip/Routes?format=json")
-    res = conn.getresponse()
-    data = json.loads(res.read())
-    
-    for item in data:
-        if route == item['Description']:
-            route = item['Route']
-
-    directions = {
+class BusHandler:
+    __metro_host__ = 'svc.metrotransit.org'
+    __dirs__ = {
             'SOUTH' : '1',
             'EAST'  : '2',
             'WEST'  : '3',
             'NORTH' : '4'
     }
 
-    direction = directions[direction.upper()]
+    def __init__(self, route, stop, direction):
+        try:
+            self.route = route
+            self.stop = stop
+            self.direction = self.__dirs__[direction.upper()]
+            self.run()
+        except KeyError:
+            print(f'{direction} is not a valid direction. Please choose between [North, South, East, West]')
 
-    conn = http.client.HTTPConnection('svc.metrotransit.org')
-    conn.request('GET', f'/NexTrip/Stops/{route}/{direction}?format=json')
-    res = conn.getresponse()
-    data = json.loads(res.read())
+    def __http_conn__(self, url):
+        conn = http.client.HTTPConnection(self.__metro_host__)
+        conn.request("GET", url)
+        res = conn.getresponse()
+        return json.loads(res.read())
 
-    for item in data:
-        if stop == item['Text']:
-            stop = item['Value']
-    
-    conn = http.client.HTTPConnection('svc.metrotransit.org')
-    conn.request('GET', f'/NexTrip/{route}/{direction}/{stop}?format=json')
-    res = conn.getresponse()
-    data = json.loads(res.read())
+    def __get_route__(self):
+        data, route = self.__http_conn__('/NexTrip/Routes?format=json'), None
+        for item in data:
+            if self.route == item['Description']:
+                route = item['Route']
+        if not route:
+            raise ValueError(f'{self.route} is not a valid Route')
+        
+        return route
 
-    print(data.pop(0)['DepartureText'])
+    def __get_stop__(self, route):
+        data, stop = self.__http_conn__(f'/NexTrip/Stops/{route}/{self.direction}?format=json'), None
+        for item in data:
+            if self.stop == item['Text']:
+                stop = item['Value']
+        if not stop:
+            raise ValueError(f'{self.stop} is not a valid Stop')
 
-except ValueError:
-    print('Input did not include {Route} {Stop} and {Direction}')
+        return stop
 
+    def __get_time__(self, route, stop):
+        data, time = self.__http_conn__(f'/NexTrip/{route}/{self.direction}/{stop}?format=json'), None
+
+        if len(data):
+            return data.pop(0)['DepartureText']
+
+    def run(self):
+        route = self.__get_route__()
+        stop = self.__get_stop__(route)
+        time = self.__get_time__(route, stop)
+        if time:
+            print(time)
+
+if __name__ == '__main__':
+
+    try:
+        if len(sys.argv) != 4: 
+            raise ValueError('Input did not include {Route} {Stop} and {Direction}') 
+        _, route, stop, direction = sys.argv
+        BusHandler(route, stop, direction)
+    except ValueError as error:
+        print(error)
